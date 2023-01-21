189a00de50be9dac1f4b6da0c0ea90e86119099f
league <ameeralih786@gmail.com>
2023-01-20 18:31:17 -0800

    I worked on Shapes and colors and Made shapes using turtle
    I pinned a moustache on an emoji
    
    I learned how to make spinny things with for loop
    
    I finished Module 0

--- _04_amazing_games/_a_terminator_cat.py
+++ _04_amazing_games/_a_terminator_cat.py
@@ -60,17 +60,17 @@ if __name__ == '__main__':
     #    e. Drag and drop the image into this python package
     
     # 2. Call the set_background() function with your variable inside of the parenthesis
-    #    for example, set_background(bg_image)
-
+    set_background("bigEyedCat.gif")
     # 3. Make a new turtle
-
+    chad: turtle.Turtle = turtle.Turtle()
     # 4. Set the turtle color and pen color to red (or any color you want)
     #    using .color('red', 'red')
-
+    chad.color('green')
+    chad.pencolor('blue')
     # 5. Set the turtle width to 0 so no outlines are drawn
-
+    chad.width(0)
     # 6. Set the turtle speed to 0 (fastest)
-
+    chad.speed(0)
     # 7. Run the program and click on one of the cat's eyes. 
     #    The x,y position of the eye will be printed at the bottom of your
     #    processing window.
@@ -80,11 +80,12 @@ if __name__ == '__main__':
     
     # 8. After you've found the x and y for the eyes create 2 eye variables
     #    and initialize them:
-    #    left_eye  = Eye(eye=my_turtle, x=-34, y=11, radius=30)
-    #    right_eye = Eye(eye=my_turtle, x=40, y=-5, radius=30)
+    left_eye = Eye(eye=chad, x=-31, y=25, radius=50)
+    right_eye = Eye(eye=chad, x=41, y=10, radius=50)
 
     # 9. Call the .draw() method on BOTH eye variables
-
+    left_eye.draw()
+    right_eye.draw()
 
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -7,22 +7,25 @@ if __name__ == '__main__':
     window.withdraw()
 
     # 1. Change this line to give you a random number between 1 - 100.
-    random_num = random.randint(1, 10)
+    num = random.randint(1, 100)
 
     # 2. Print out the random variable that you made in step #1
-
+    print(num)
     # 3. Code a for loop to run steps 4-10, 10 times
-
+    for i in range(10):
         # 4. Ask the user for a guess using a pop-up window, and save their response
-
+        hi = simpledialog.askinteger(title=None, prompt="Guess a number between 1 and 100")
         # 5. If the guess is correct
+        if hi == num:
             # 6. Win. Use 'sys.exit(0)' to end the program
-
+            sys.exit(0)
         # 7. if the guess is high
+        if hi > num:
+            simpledialog.askstring(title=None, prompt="try again: lower")
             # 8. Tell them it's too high
         # 9. Else if the guess is low
             # 10. Tell them it's too low
-
-    #11. Outside of the loop, tell the user they lost
-
+        if hi < num:
+            simpledialog.askstring(title=None, prompt="try again: higher")
     window.mainloop()
+    messagebox.showinfo("YOU LOSE")
